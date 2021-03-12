39e09ccee1bfae6271f97dc19fd1a61702c7f67a
lethanhdat25 <70951430+lethanhdat25@users.noreply.github.com>
2021-03-12 10:24:14 +0700

    @@Menden

--- src/assignment/Practical/Main.java
+++ src/assignment/Practical/Main.java
@@ -6,15 +6,15 @@ public class Main {
         int a=0;
         Hotel hotel=new Hotel();
         hotel.add("Hotel1","HN","Hung");
-        hotel.add("Hotel2","HN","Quang");
+        hotel.add("Hotel2","HCM","Quang");
         hotel.add("Hotel3","HN","Lam");
-        hotel.add("Hotel4","HN","Manh");
-        hotel.add("Hotel5","HN","Yen");
+        hotel.add("Hotel4","Hai Phong","Manh");
+        hotel.add("Hotel5","DN","Yen");
         hotel.add("Hotel6","HN","Khanh");
-        hotel.add("Hotel7","HN","Thanh");
+        hotel.add("Hotel7","Nha trang","Thanh");
         hotel.add("Hotel8","HN","Dat");
-        hotel.add("Hotel9","HN","Chien");
-        hotel.add("Hotel10","HN","Bao");
+        hotel.add("Hotel9","Nghe An","Chien");
+        hotel.add("Hotel10","Thanh Hoa","Bao");
         for (Hotel x:
              hotel.listHotel) {
             System.out.println("Ten khach san: "+x.name+", dia chi: "+x.location+", ten chu khach san: "+x.ownerName);
