0668a7b314b9828b414924828a21ec8396d0ccd9
lethanhdat25 <70951430+lethanhdat25@users.noreply.github.com>
2021-03-12 10:21:33 +0700

    @@Menden

--- src/assignment/Practical/Main.java
+++ src/assignment/Practical/Main.java
@@ -5,16 +5,16 @@ public class Main {
     public static void main(String[] args){
         int a=0;
         Hotel hotel=new Hotel();
-        hotel.add("Khachsan1","HN","Hung");
-        hotel.add("Khachsan2","HN","Quang");
-        hotel.add("Khachsan3","HN","Lam");
-        hotel.add("Khachsan4","HN","Manh");
-        hotel.add("Khachsan5","HN","Yen");
-        hotel.add("Khachsan6","HN","Khanh");
-        hotel.add("Khachsan7","HN","Thanh");
-        hotel.add("Khachsan8","HN","Dat");
-        hotel.add("Khachsan9","HN","Chien");
-        hotel.add("Khachsan10","HN","Bao");
+        hotel.add("Hotel1","HN","Hung");
+        hotel.add("Hotel2","HN","Quang");
+        hotel.add("Hotel3","HN","Lam");
+        hotel.add("Hotel4","HN","Manh");
+        hotel.add("Hotel5","HN","Yen");
+        hotel.add("Hotel6","HN","Khanh");
+        hotel.add("Hotel7","HN","Thanh");
+        hotel.add("Hotel8","HN","Dat");
+        hotel.add("Hotel9","HN","Chien");
+        hotel.add("Hotel10","HN","Bao");
         for (Hotel x:
              hotel.listHotel) {
             System.out.println("Ten khach san: "+x.name+", dia chi: "+x.location+", ten chu khach san: "+x.ownerName);
